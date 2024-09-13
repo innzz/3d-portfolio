4417f1dc513fcff2a8b5b1cb5114259b8f69ca26
inzamam <inzamam.pawaskar10@gmail.com>
2024-09-13 20:00:04 +0530

    Built Hero Section

--- src/constants/index.js
+++ src/constants/index.js
@@ -255,22 +255,22 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
       : isMobile
       ? [5, -5, 0]
       : isTablet
-      ? [5, -5, 0]
+      ? [7, -7, 0]
       : [9, -5.5, 0],
     reactLogoPosition: isSmall
       ? [3, 4, 0]
       : isMobile
-      ? [5, 4, 0]
+      ? [5, 3, 0]
       : isTablet
-      ? [5, 4, 0]
-      : [12, 3, 0],
+      ? [5, 3, 0]
+      : [9, 3, 0],
     ringPosition: isSmall
       ? [-5, 7, 0]
       : isMobile
-      ? [-10, 10, 0]
+      ? [-12, 6, 0]
       : isTablet
-      ? [-12, 10, 0]
-      : [-24, 10, 0],
+      ? [-14, 6, 0]
+      : [-20, 6, 0],
     targetPosition: isSmall
       ? [-5, -10, -10]
       : isMobile
